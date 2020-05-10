f7d867ef146d776a8f1361e83019d9e2c437670b
Ben Morrison <ben@gbmor.dev>
2020-05-10 14:27:11 -0400

    fixed bug in userCountQuery
    was only returning a single user when there were multiple users.
    moved to absolute path for ls and strings.Fields().

--- usercount.go
+++ usercount.go
@@ -9,12 +9,12 @@ import (
 // Just returns the number of directories in /home
 // The assumption being, it's the number of human users.
 func userCountQuery(format string) ([]byte, error) {
-	ls, err := exec.Command("ls", "/home").Output()
+	ls, err := exec.Command("/bin/ls", "/home").Output()
 	if err != nil {
 		return nil, fmt.Errorf("Couldn't execute ls: %w", err)
 	}
 
-	split := strings.Split(string(ls), " ")
+	split := strings.Fields(string(ls))
 
 	if format == "plain" {
 		return []byte(fmt.Sprintf("%v users", len(split))), nil

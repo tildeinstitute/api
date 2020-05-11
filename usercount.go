65986b345f9e8ecf7e75f58fc1b2936a397e9520
Ben Morrison <ben@gbmor.dev>
2020-05-11 02:34:19 -0400

    ignoring hidden directories in user count, changed error message to specify it came from user count query

--- usercount.go
+++ usercount.go
@@ -11,10 +11,18 @@ import (
 func userCountQuery(format string) ([]byte, error) {
 	ls, err := exec.Command("/bin/ls", "/home").Output()
 	if err != nil {
-		return nil, fmt.Errorf("Couldn't execute ls: %w", err)
+		return nil, fmt.Errorf("User Count Query: %w", err)
 	}
 
 	split := strings.Fields(string(ls))
+	total := 0
+
+	for _, e := range split {
+		if strings.HasPrefix(e, ".") || strings.HasPrefix(e, "_") {
+			continue
+		}
+		total++
+	}
 
 	if format == "plain" {
 		return []byte(fmt.Sprintf("%v users\n", len(split))), nil
@@ -23,7 +31,7 @@ func userCountQuery(format string) ([]byte, error) {
 	out := fmt.Sprintf(`{
 	"userCount": "%v"
 }
-`, len(split))
+`, total)
 
 	return []byte(out), nil
 }
